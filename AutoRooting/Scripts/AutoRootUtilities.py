ae20be1f4540b14fba3170601d9ae7248210aeff
Maxwelldoug <maxmacneill@hotmail.ca>
2023-01-04 12:47:17 -0400

    Finished Windows Setup - I think.
    Also completed review of the new code in the Utility Script.

--- AutoRooting/Scripts/AutoRootUtilities.py
+++ AutoRooting/Scripts/AutoRootUtilities.py
@@ -1,46 +1,50 @@
-
-
-
-#[AutoRootUtiilities.py] file will contain several functions, where mostly of them are required for the Setup.py and for Main.py files
+# [AutoRootUtiilities.py] file will contain several functions, where mostly of them are required for the Setup.py and for Main.py files
 import os, platform, ctypes, shutil, socket, urllib.request, zipfile
 
 
-#Colors for String formatting :
+# Colors for String formatting :
 Colors = {
-    "Reset": '\033[0m',
-    "Grey": '\033[1;30;48m',
-    "Red": '\033[1;31;48m',
-    "Green": '\033[1;32;48m',
-    "Yellow": '\033[1;33;48m',
-    "Blue": '\033[1;34;48m', 
-    "Magenta": '\033[1;35;48m',
-    "Cyan": '\033[1;36;48m',
-    "White": '\033[1;37;48m'
+    "Reset": "\033[0m",
+    "Grey": "\033[1;30;48m",
+    "Red": "\033[1;31;48m",
+    "Green": "\033[1;32;48m",
+    "Yellow": "\033[1;33;48m",
+    "Blue": "\033[1;34;48m",
+    "Magenta": "\033[1;35;48m",
+    "Cyan": "\033[1;36;48m",
+    "White": "\033[1;37;48m",
 }
-#Usage Example : print(Colors["Red"] + 'Color that string' + Colors["Reset"])
+# Usage Example : print(Colors["Red"] + 'Color that string' + Colors["Reset"])
 
 
-#Prompts the user (y/n)
+# Prompts the user (y/n)
 def askUser(question) -> bool:
     while "the answer is invalid":
-        reply = str(input(question + Colors["Green"] + ' (Y/N) : ' + Colors["Reset"])).lower().strip()
-        if reply[:1] == 'y':
+        reply = (
+            str(input(question + Colors["Green"] + " (Y/N) : " + Colors["Reset"]))
+            .lower()
+            .strip()
+        )
+        if reply[:1] == "y":
             return True
-        if reply[:1] == 'n':
+        if reply[:1] == "n":
             return False
 
 
 class UnsupportedPlatformError(Exception):
     pass
 
-# getPlatform returns variables for supported platforms. 
+
+# getPlatform returns variables for supported platforms.
 # Windows systems will return 1, while Linux systems return 2. All other systems will error out.
-def getPlatform() -> int: 
+def getPlatform() -> int:
     if platform.system() == "Windows":
         return 1
     elif platform.system() == "Linux":
         return 2
-    raise UnsupportedPlatformError(f"{Colors['Red']}Unsupported Platform! {Colors['Reset']}\nOnly Windows or Linux are supported.\nIf you are on a Windows or Linux machine, please report this error.")
+    raise UnsupportedPlatformError(
+        f"{Colors['Red']}Unsupported Platform! {Colors['Reset']}\nOnly Windows or Linux are supported.\nIf you are on a Windows or Linux machine, please report this error."
+    )
 
 
 # isSetup returns true if setup has been run, or false otherwise. It's that simple.
@@ -48,16 +52,17 @@ def isSetup() -> bool:
     wd = os.getcwd() + "\\Tools\\config.cfg"
     if os.path.isfile(wd):
         return True
-    else: 
+    else:
         return False
 
 
 # isElevated is the first program here that can actually fail to get a result. This exception is defined for that case.
-class AdminStateUnknownError(Exception): 
+class AdminStateUnknownError(Exception):
     pass
 
-#Checks if the script has Elevated Priviledges. On Linux, this means uid 0, while on wnidows we check if user is an admin.
-def isElevated(): 
+
+# Checks if the script has Elevated Priviledges. On Linux, this means uid 0, while on wnidows we check if user is an admin.
+def isElevated():
     try:
         return os.getuid() == 0
     except AttributeError:
@@ -68,86 +73,99 @@ def isElevated():
         raise AdminStateUnknownError("Cannot determine whether the user is an admin!")
 
 
-#Returns the free disk space, in bytes
-def getDiskSpace(): 
+# Returns the free disk space, in bytes
+def getDiskSpace():
     total, used, free = shutil.disk_usage(os.getcwd())
     return free
 
 
-#Checks if dns exists and can ping a known good server
+# Checks if dns exists and can ping a known good server
 def isConnected() -> bool:
-    print(f"\nRunning {Colors['Red']}Internet Connection Test{Colors['Reset']}...")
-    
-    print(f"{Colors['Red']}Connection Status{Colors['Reset']} : ", end='')
     REMOTE_SERVER = "one.one.one.one"
-    try: 
+    try:
         host = socket.gethostbyname(REMOTE_SERVER)
         s = socket.create_connection((host, 80), 2)
         s.close()
-        print(f"{Colors['Green']}Online{Colors['Reset']}")
         return True
-    except Exception: 
+    except Exception:
         pass
-
-    print(f"{Colors['Green']}Offline{Colors['Reset']}")
     return False
 
 
-#Checks if whatever executable provided (as string) exists in $PATH
-def checkTool(name: str) -> bool: 
+# Checks if whatever executable provided (as string) exists in $PATH
+def checkTool(name: str) -> bool:
     return shutil.which(name) is not None
-
 
 
 class DownloadFailedError(Exception):
     pass
 
-TryAgainTimes = 0 #This lets the code know that the download has been re-started more than N times
-def download(URLink: str, FileName: str): #Downloads a file
+
+TryAgainTimes = (
+    0  # This lets the code know that the download has been re-started more than N times
+)
+
+
+def download(URLink: str, FileName: str):  # Downloads a file
     DestinationPath = os.getcwd() + "\\Downloads\\" + FileName
     try:
-        print(f"{Colors['Green']}\nDownloading{Colors['Reset']} {FileName} to {DestinationPath}")
+        print(
+            f"{Colors['Green']}\nDownloading{Colors['Reset']} {FileName} to {DestinationPath}"
+        )
         urllib.request.urlretrieve(URLink, DestinationPath)
         print(f"{Colors['Green']}\tDone!{Colors['Reset']}")
 
-    except urllib.error.ContentTooShortError: #If the download has been interrupted for Connection Lost or the connection was Forcibly closed
+    except urllib.error.ContentTooShortError:  # If the download has been interrupted for Connection Lost or the connection was Forcibly closed
         print(f"{Colors['Red']}Failed{Colors['Reset']} to download {FileName}...")
-        
+
         if not isConnected():
             print("\t[Please make sure you are connected to Internet!]")
-            Ok_IAmConnected = input("Press 'Enter' key if you are connected to Internet : ") #This input is like a delay : when the user is correctly connected to internet then the program will continue
+            Ok_IAmConnected = input(
+                "Press 'Enter' key if you are connected to Internet : "
+            )  # This input is like a delay : when the user is correctly connected to internet then the program will continue
             download(URLink, FileName)
 
-        elif isConnected(): #If the connection is stable then the connection was Forcibly closed by AntiVirus or an other process.
+        elif (
+            isConnected()
+        ):  # If the connection is stable then the connection was Forcibly closed by AntiVirus or an other process.
             print("\t[The download process has been stopped from an unknown source!]")
-            
-            if askUser(f"\Dooo you want try again to download {FileName} ? "):
+
+            if askUser("Would you like to retry?"):
                 TryAgainTimes += 1
                 if TryAgainTimes == 2:
-                    raise DownloadFailedError(Colors["Red"] + f"[The file {FileName} has failed to be downloaded too many times...]" + Colors["Reset"])
+                    raise DownloadFailedError(
+                        Colors["Red"]
+                        + f"[The download has failed too many times. Exiting.]"
+                        + Colors["Reset"]
+                    )
 
                 download(URLink, FileName)
             else:
-                raise DownloadFailedError("The user stopped the download process")
+                raise DownloadFailedError(
+                    "The user stopped the download process - Execution cannot continue."
+                )
     except:
-        raise DownloadFailedError(f"The {FileName} failed to be downloaded for some reason.\nThe application cannot continue!")
+        raise DownloadFailedError(
+            f"{FileName} failed to be downloaded for some reason.\nThe application cannot continue."
+        )
 
-    
     TryAgainTimes = 0
 
 
-
-#Extracts a zip
+# Extracts a zip
 def extractZip(Zip_FileName: str, DestinationPath: str):
-    print(f"{Colors['Green']}Extracting{Colors['Reset']} {Zip_FileName} to {DestinationPath}", end = '')
+    print(
+        f"{Colors['Green']}Extracting{Colors['Reset']} {Zip_FileName} to {DestinationPath}",
+        end="",
+    )
 
-    with zipfile.ZipFile(Zip_FileName, 'r') as zip_ref:
+    with zipfile.ZipFile(Zip_FileName, "r") as zip_ref:
         zip_ref.extractall(DestinationPath)
     print(f"{Colors['Green']}\tDone!{Colors['Reset']}")
 
 
-#This checks for the existance of a file within the AutoRooting directory, using relative paths. 
-#Inputs should be formatted so AutoRooting/Downloads/Dockerfile is input as Downloads/Dockerfile
+# This checks for the existance of a file within the AutoRooting directory, using relative paths.
+# Inputs should be formatted so AutoRooting/Downloads/Dockerfile is input as Downloads/Dockerfile
 def checkfile(filename: str):
     basepath = os.getcwd() + "\\"
     if os.path.isfile(basepath + filename):
--- AutoRooting/Setup.py
+++ AutoRooting/Setup.py
@@ -1,6 +1,6 @@
 import Scripts.AutoRootUtilities as rutil
 
-#We start by checking everything is configured correctly OK
+# We start by checking everything is configured correctly OK
 
 mode = rutil.getPlatform()
 
@@ -10,75 +10,131 @@ elif mode == 2:
     print("Detected a Linux system")
 
 if not rutil.getDiskSpace() > 8589934592:
-    raise SystemExit("Less than 8GiB of disk space is available, setup cannot continue.")
+    raise SystemExit(
+        "Less than 8GiB of disk space is available, setup cannot continue."
+    )
 
 if not rutil.isElevated():
-    raise SystemExit("This Script requires elevated Priviledges, Execution cannot continue.")
+    raise SystemExit(
+        "This Script requires Elevated Priviledges, Execution cannot continue."
+    )
 
 if not rutil.isConnected():
-    raise SystemExit("Could not connect to the Internet, or could not resolve DNS. Check your Internet.")
+    raise SystemExit(
+        "Could not connect to the Internet, or could not resolve DNS. Check your Internet."
+    )
 
 resumeSetup = False
+if rutil.os.path.isfile("Tools\\inprogress.cfg"):
+    print("Resuming setup")
+    resumeSetup = True
 
 if not rutil.isSetup():
-    if mode == 1:
+    if (mode == 1) and (not resumeSetup):
         if not rutil.checkTool("wsl"):
-            raise SystemExit("This tool requires WSL2 to be installed in order to patch magisk boot images with docker. WSL2 can be installed on all versions of Windows starting with Windows 10 2004 and all releases of Windows 11. For more information, see https://aka.ms/wsl")
-        if rutil.os.path.isfile("Tools\\inprogress.cfg"):
-            print("Resuming setup")
-            resumeSetup = True
+            raise SystemExit(
+                "This tool requires WSL2 to be installed in order to patch magisk boot images with docker. WSL2 can be installed on all versions of Windows starting with Windows 10 2004 and all releases of Windows 11. For more information, see https://aka.ms/wsl"
+            )
         if rutil.checkTool("adb") or rutil.checkTool("fastboot"):
-            raise SystemExit("ADB or Fastboot are already in Path. This will conflict with the installed version, which will install the latest version of platform-tools into path. Please remove the offending programs and re-run Setup.")
+            raise SystemExit(
+                "ADB or Fastboot are already in Path. This will conflict with the installed version, which will install the latest version of platform-tools into path. Please remove the offending programs and re-run Setup."
+            )
         if not resumeSetup:
             print("Checks Complete, Detected first run or configuration removed.")
-            print("Welcome to Setup.py, this setup file will install google's platform-tools and docker, then will spawn a docker container to patch boot images for magisk.")
-            if (not rutil.askUser("Is this alright?")):
+            print(
+                "Welcome to Setup.py, this setup file will install google's platform-tools and docker, then will spawn a docker container to patch boot images for magisk."
+            )
+            if not rutil.askUser("Is this alright?"):
                 raise SystemExit("User refused setup, exiting.")
             else:
                 print("Preparing File Structure.")
-                try: 
+                try:
                     rutil.os.mkdir("Tools")
                 except FileExistsError:
                     pass
-                try: 
+                try:
                     rutil.os.mkdir("Tools\\magiskDocker")
                 except FileExistsError:
-                    pass 
-                try: 
+                    pass
+                try:
                     rutil.os.mkdir("Downloads")
                 except FileExistsError:
                     pass
-                print("Prepared - Downloading platform-tools")
-                rutil.download("https://dl.google.com/android/repository/platform-tools-latest-windows.zip", "plattools.zip")
+                rutil.download(
+                    "https://dl.google.com/android/repository/platform-tools-latest-windows.zip",
+                    "plattools.zip",
+                )
                 print("Download Complete - Extracting")
                 rutil.extractZip("Downloads\\plattools.zip", "Tools")
-                rutil.os.system("setx PATH \"" + rutil.os.getcwd() +"\\Tools\platform-tools;%PATH%\"")
+                rutil.os.system(
+                    'setx PATH "' + rutil.os.getcwd() + '\\Tools\platform-tools;%PATH%"'
+                )
                 print("Platform Tools Installed and added to PATH - Cleaning up")
                 rutil.os.remove("Downloads\\plattools.zip")
-                #Docker installer will be run with `"Docker Desktop Installer.exe" install --accept-license`
+                # Docker installer will be run with `"Docker Desktop Installer.exe" install --accept-license`
                 if not rutil.checkTool("docker"):
-                    print("Downloading Docker for Windows installer")
-                    rutil.download("https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe", "docker-inst.exe")
-                    print("Download Complete - Preparing to Install")
-                    print("The Docker Installer will now run. please follow its process to completion with the default install location. Do not log out of Windows. Instead, return here and report when it has succeeded.")
+                    rutil.download(
+                        "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe",
+                        "docker-inst.exe",
+                    )
+                    print(
+                        "The Docker Installer will now run. please follow its process to completion with the default install location. Do not log out of Windows. Instead, return here and report when it has succeeded."
+                    )
                     rutil.os.system("Downloads\\docker-inst.exe --accept-license")
                     rutil.askUser()
                     rutil.os.remove("Downloads\\docker-inst.exe")
                     rutil.os.create()
-                    with open('Tools\\inprogress.cfg', 'w') as fp:
+                    with open("Tools\\inprogress.cfg", "w") as fp:
                         pass
-                    raise SystemExit("Phase 1 of Setup is complete. Please Reboot your computer and re-run this program to continue setup.")
-                else: 
+                    raise SystemExit(
+                        "Phase 1 of Setup is complete. Please Reboot your computer and re-run this program to continue setup."
+                    )
+                else:
                     print("Docker already installed - Skipping")
-            if resumeSetup or rutil.checkTool("docker"):
-                print("Resuming setup - Preparing Magisk - Docker Desktop will start, this is currently required in order to start the Docker Daemon. Please confirm when it finishes loading")
-                rutil.os.system("start /w \"\" \"C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe\"")
-                rutil.askUser()
-                #Docker is a pain in the ass. I'm going to leave it here until I can figure out how to make Magisk-In-A-Box work properly.
+    if resumeSetup or rutil.checkTool("docker"):
+        print(
+            "Resuming setup - Preparing Magisk - Docker Desktop will start, this is currently required in order to start the Docker Daemon. Please confirm when it finishes loading"
+        )
+        rutil.os.system(
+            'start /w "" "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"'
+        )
+        rutil.askUser("")
+        rutil.download(
+            "https://raw.githubusercontent.com/xclusivor/magisk-in-a-box/main/Dockerfile",
+            "Dockerfile",
+        )
+        rutil.download(
+            "https://raw.githubusercontent.com/xclusivor/magisk-in-a-box/main/miab.sh",
+            "miab.sh",
+        )
+        rutil.os.replace("Downloads\\Dockerfile", "Tools\\magiskDocker\\Dockerfile")
+        rutil.os.replace("Downloads\\miab.sh", "Tools\\magiskDocker\\miab.sh")
+        print(
+            "Docker will now attempt to build the magisk image. Be patient, this may take up to an hour or as little as 5 minutes, depending on your System specs"
+        )
+        rutil.os.system("docker build .\\Tools\\magiskDocker -t miab")
+        if rutil.askUser(
+            "Did Docker build the image successfully? (All output is not red, and no Errors present)"
+        ):
+            with open("Tools\\config.cfg", "w") as fp:
+                fp.write(
+                    "This file exists to validate that setup has been completed - Do not move or delete it."
+                )
+            rutil.os.remove("Tools\\inprogress.cfg")
+            print("Setup is now complete!")
+        else:
+            with open("Tools\\inprogress.cfg", "w") as fp:
+                pass
+            raise SystemExit(
+                "Docker failed to compile - setup cannot continue. Please resolve the above errors and re-run setup."
+            )
+
     elif mode == 2:
         print("Linux System Setup is currently not implemented. Sorry!")
-    #TODO: Do more setup
-    print("To configure device specific options, install drivers, or update any of the installed utilities, run this program again.")
+    # TODO: Do more setup
+    print(
+        "To configure device specific options, install drivers, or update any of the installed utilities, run this program again."
+    )
 else:
     print("Setup has already been completed - Opening configuration menu")
-    #TODO: Configuration Menu
+    # TODO: Configuration Menu
