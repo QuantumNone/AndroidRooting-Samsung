4000be2b4fa498d08aa37f218412705e1df13f10
Maxwelldoug <maxmacneill@hotmail.ca>
2023-01-03 22:28:23 -0400

    Implemented Platform-Tools installation

--- AutoRooting/Scripts/AutoRootUtilities.py
+++ AutoRooting/Scripts/AutoRootUtilities.py
@@ -1,30 +1,45 @@
+
+
+
 #This file will contain several shared utilities for use in the main classes, such as Main.py and Setup.py
 
-import os, platform, ctypes, shutil, socket
+import os, platform, ctypes, shutil, socket, urllib.request, zipfile
+
+#Prompts the user (y/n)
+def askUser(question):
+    while "the answer is invalid":
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[:1] == 'y':
+            return True
+        if reply[:1] == 'n':
+            return False
+
+class UnsupportedPlatformError(Exception):
+    pass
 
 # getPlatform returns variables for supported platforms. 
-# Windows systems will return 1, while Linux systems return 2. All other systems will return 0.
-def getPlatform(): 
+# Windows systems will return 1, while Linux systems return 2. All other systems will error out.
+def getPlatform() -> int: 
     if platform.system() == "Windows":
         return 1
     elif platform.system() == "Linux":
         return 2
-    else:
-        return 0
+    raise UnsupportedPlatformError("This Platform is not currently supported. Only Windows or Linux are supported. If you are on a Windows or Linux machine, please report this error.")
 
 # isSetup returns true if setup has been run, or false otherwise. It's that simple.
-def isSetup():
+def isSetup() -> bool:
     wd = os.getcwd() + "\\Tools"
-    if os.path.isfile(wd):
+    if os.path.exists(wd):
         return True
     else: 
         return False
 
-class AdminStateUnknownError(Exception): #Is Elevated is the first program here that can actually fail to get a result. This exception is defined for that case.
-    """Cannot determine whether the user is an admin."""
+# isElevated is the first program here that can actually fail to get a result. This exception is defined for that case.
+class AdminStateUnknownError(Exception): 
     pass
 
-def isElevated(): #Checks if the script has Elevated Priviledges. On Linux, this means uid 0, while on wnidows we check if user is an admin.
+# Checks if the script has Elevated Priviledges. On Linux, this means uid 0, while on wnidows we check if user is an admin.
+def isElevated(): 
     try:
         return os.getuid() == 0
     except AttributeError:
@@ -32,13 +47,15 @@ def isElevated(): #Checks if the script has Elevated Priviledges. On Linux, this
     try:
         return ctypes.windll.shell32.IsUserAnAdmin() == 1
     except AttributeError:
-        raise AdminStateUnknownError
+        raise AdminStateUnknownError("Cannot determine whether the user is an admin.")
 
-def getDiskSpace(): #returns the free disk space, in bytes
+#returns the free disk space, in bytes
+def getDiskSpace(): 
     total, used, free = shutil.disk_usage(os.getcwd())
     return free
 
-def isConnected(): #checks if dns exists and can ping a known good server
+#checks if dns exists and can ping a known good server
+def isConnected() -> bool: 
     REMOTE_SERVER = "one.one.one.one"
     try: 
         host = socket.gethostbyname(REMOTE_SERVER)
@@ -49,5 +66,37 @@ def isConnected(): #checks if dns exists and can ping a known good server
         pass
     return False
 
-def checkTool(name): #checks if whatever executable provided (as string) exists in $PATH
+#checks if whatever executable provided (as string) exists in $PATH
+def checkTool(name): 
     return shutil.which(name) is not None
+
+class DownloadFailedError(Exception):
+    pass
+
+#Downloads a file
+def download(URLink: str, FileName: str):
+    DestinationPath = os.getcwd()
+    try:
+        urllib.request.urlretrieve(URLink, DestinationPath + "\\Downloads\\" + FileName)
+    except urllib.error.ContentTooShortError:
+        if askUser("The download was interrupted, either because connection was lost or the connection was forcibly closed. Would you like to retry the download?"):
+            download(URLink, FileName)
+        else:
+            raise DownloadFailedError("The Download failed for some reason. This Application cannot continue.")
+    except:
+        raise DownloadFailedError("The Download failed for some reason. This Application cannot continue.")
+
+
+
+#Extracts a zip
+def extractZip(zip_file: str, DestinationPath: str):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(DestinationPath)
+
+#This checks for the existance of a file witin the AutoRooting directory, using relative paths. Inputs should be formatted so AutoRooting/Downloads/Dockerfile is input as Downloads/Dockerfile
+def checkfile(filename: str):
+    basepath = os.getcwd()+"\\"
+    if os.path.isfile(basepath+filename):
+        return True
+    else:
+        return False
--- AutoRooting/Setup.py
+++ AutoRooting/Setup.py
@@ -4,10 +4,49 @@ import Scripts.AutoRootUtilities as rutil
 
 mode = rutil.getPlatform()
 
+if mode == 1:
+    print("Detected a Windows system")
+elif mode == 2:
+    print("Detected a Linux system")
 
+if not rutil.getDiskSpace() > 8589934592:
+    raise SystemExit("Less than 8GiB of disk space is available, setup cannot continue.")
+
+if not rutil.isElevated():
+    raise SystemExit("This Script requires elevated Priviledges, Execution cannot continue.")
+
+if not rutil.isConnected():
+    raise SystemExit("Could not connect to the Internet, or could not resolve DNS. Check your Internet.")
 
 if not rutil.isSetup():
-    #TODO: Do setup
+    if mode == 1:
+        if not rutil.checkTool("wsl"):
+            raise SystemExit("This tool requires WSL2 to be installed in order to patch magisk boot images with docker. WSL2 can be installed on all versions of Windows starting with Windows 10 2004 and all releases of Windows 11. For more information, see https://aka.ms/wsl")
+        if rutil.checkTool("adb") or rutil.checkTool("fastboot"):
+            raise SystemExit("ADB or Fastboot are already in Path. This will conflict with the installed version, which will install the latest version of platform-tools into path. Please remove the offending programs and re-run Setup.")
+        print("Checks Complete, Detected first run or configuration removed.")
+        print("Welcome to Setup.py, this setup file will install google's platform-tools and docker, then will spawn a docker container to patch boot images for magisk.")
+        if not rutil.askUser("Is this alright?"):
+            raise SystemExit("User refused setup, exiting.")
+        else:
+            print("Preparing File Structure.")
+            try: 
+                rutil.os.mkdir("Tools")
+            except FileExistsError:
+                pass
+            try: 
+                rutil.os.mkdir("Tools\\magiskDocker")
+            except FileExistsError:
+                pass 
+            try: 
+                rutil.os.mkdir("Downloads")
+            except FileExistsError:
+                pass 
+            rutil.download("https://dl.google.com/android/repository/platform-tools-latest-windows.zip", "plattools.zip")
+            rutil.extractZip("Downloads\\plattools.zip", "Tools")
+            rutil.os.remove("Downloads\\plattools.zip")
+            print(rutil.os.system("setx PATH \"" + rutil.os.getcwd() +"\\Tools\platform-tools;%PATH%\""))
+    #TODO: Do more setup
     print("To configure device specific options and drivers, or to update any of the installed utilities, run this program again.")
 else:
     print("Setup has been completed - Opening configuration menu")
