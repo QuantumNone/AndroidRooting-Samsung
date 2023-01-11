1c249ce06994a42336e764ebe4332b89e8124bf4
QuantumNone <jeamscrush@gmail.com>
2023-01-11 23:28:11 +0100

    Testing...

--- AutoRooting/Scripts/Testing.py
+++ AutoRooting/Scripts/Testing.py
@@ -1,14 +1,40 @@
-import subprocess
+from AutoRootUtilities import Colors, sleep, sleepDelay
 
-def Check_AdbConnection() -> bool:
-    try:
-        AdbDevices_output = subprocess.check_output("adb devices", stderr = subprocess.STDOUT, shell = True).strip()
-        print(AdbDevices_output[-6:])
-        if AdbDevices_output[-6:] == b'device':
-            return True
-        else:
-            return False
-    except subprocess.CalledProcessError:
-        pass
+def SetupDeviceForUSBCommunication():
+    """User has to MANUALLY setup his device to start USB communication"""
 
-print(Check_AdbConnection())
+    def DelayedPrint(string: str, sleepDelay: float) -> None:
+        string = string.split('\n')
+        for line in string:
+            print(line)
+            sleep(sleepDelay)
+
+    instructions = f'''
+    1. Open your device {Colors["Green"]}settings{Colors["Reset"]} and navigate into "About my phone" option.
+    2. Search for "{Colors["Red"]}Build number{Colors["Reset"]}" option inside these settings (if you cannot find it try in "{Colors["Green"]}Software Information{Colors["Reset"]}" option).
+    3. Tap 7 times on "Build number" option to enable {Colors["Red"]}Developer Options{Colors["Reset"]}.
+    4. Go back to settings and {Colors["Red"]}search{Colors["Reset"]} for Developer Options.
+    5. Search for "{Colors["Red"]}USB debugging{Colors["Reset"]}" option and {Colors["Green"]}enable{Colors["Reset"]} it.
+    6. {Colors["Green"]}Connect{Colors["Reset"]} now your device to your computer trough USB cable and check your device screen.
+    7. {Colors["Green"]}Allow{Colors["Reset"]} the pop-up asking for computer permissions.
+    8. Now search inside Developer Options for "{Colors["Red"]}Select USB configuration{Colors["Reset"]}".
+    9. Click it and select "{Colors["Green"]}MTP File transfer{Colors["Reset"]}" protocol.'''
+
+    DelayedPrint(instructions, sleepDelay)
+
+    input(f'\n\t=> Press {Colors["Green"]}Enter{Colors["Reset"]} key to continue : ')
+
+    print(f'\n    10. Now search for "{Colors["Red"]}OEM Unlocking{Colors["Reset"]}" option in Developer options and {Colors["Green"]}ENABLE{Colors["Reset"]} it!')
+    print(
+        f'''
+        {Colors["Red"]}IF{Colors["Reset"]} YOU CANNOT FIND THAT OPTION THEN LOOK AT THESE DOCUMENTATION :
+        \t1. "{Colors["Blue"]}https://krispitech.com/fix-the-missing-oem-unlock-in-developer-options/{Colors["Reset"]}"
+        \t2. "{Colors["Blue"]}https://www.quora.com/Why-do-some-mobile-companies-refuse-to-unlock-bootloaders-like-Huawei-and-Realme{Colors["Reset"]}"
+        '''
+        )
+
+    input(f'\n\t=> Press {Colors["Green"]}Enter{Colors["Reset"]} key to continue : ')
+    print()
+
+
+SetupDeviceForUSBCommunication()
