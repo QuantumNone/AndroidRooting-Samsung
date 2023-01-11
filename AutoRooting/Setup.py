45ea020c0e531b17256629fd1e411566661fa6cb
QuantumNone <jeamscrush@gmail.com>
2023-01-11 23:28:00 +0100

    Colors dict improved

--- AutoRooting/Setup.py
+++ AutoRooting/Setup.py
@@ -1,17 +1,7 @@
 import Scripts.AutoRootUtilities as rutil
  
 # Colors for String formatting :
-Colors = {
-    "Reset": "\033[0m",
-    "Grey": "\033[1;30m",
-    "Red": "\033[1;31m",
-    "Green": "\033[1;32m",
-    "Yellow": "\033[1;33m",
-    "Blue": "\033[1;34m",
-    "Magenta": "\033[1;35m",
-    "Cyan": "\033[1;36m",
-    "White": "\033[1;37m",
-}
+Colors = rutil.Colors
 # Usage Example : print(Colors["Red"] + 'Color that string' + Colors["Reset"])
 
 
