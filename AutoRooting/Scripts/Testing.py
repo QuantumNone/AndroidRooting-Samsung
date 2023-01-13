0248b0c6856d0bd63ab45b88dccb62b1911c9b1a
QuantumNone <jeamscrush@gmail.com>
2023-01-13 18:07:23 +0100

    How Samloader should work
    Just edit line 9 with the device's firmware
     informations you want to download.

--- AutoRooting/Scripts/Testing.py
+++ AutoRooting/Scripts/Testing.py
@@ -1,40 +1,44 @@
-from AutoRootUtilities import Colors, sleep, sleepDelay
-
-def SetupDeviceForUSBCommunication():
-    """User has to MANUALLY setup his device to start USB communication"""
-
-    def DelayedPrint(string: str, sleepDelay: float) -> None:
-        string = string.split('\n')
-        for line in string:
-            print(line)
-            sleep(sleepDelay)
-
-    instructions = f'''
-    1. Open your device {Colors["Green"]}settings{Colors["Reset"]} and navigate into "About my phone" option.
-    2. Search for "{Colors["Red"]}Build number{Colors["Reset"]}" option inside these settings (if you cannot find it try in "{Colors["Green"]}Software Information{Colors["Reset"]}" option).
-    3. Tap 7 times on "Build number" option to enable {Colors["Red"]}Developer Options{Colors["Reset"]}.
-    4. Go back to settings and {Colors["Red"]}search{Colors["Reset"]} for Developer Options.
-    5. Search for "{Colors["Red"]}USB debugging{Colors["Reset"]}" option and {Colors["Green"]}enable{Colors["Reset"]} it.
-    6. {Colors["Green"]}Connect{Colors["Reset"]} now your device to your computer trough USB cable and check your device screen.
-    7. {Colors["Green"]}Allow{Colors["Reset"]} the pop-up asking for computer permissions.
-    8. Now search inside Developer Options for "{Colors["Red"]}Select USB configuration{Colors["Reset"]}".
-    9. Click it and select "{Colors["Green"]}MTP File transfer{Colors["Reset"]}" protocol.'''
-
-    DelayedPrint(instructions, sleepDelay)
-
-    input(f'\n\t=> Press {Colors["Green"]}Enter{Colors["Reset"]} key to continue : ')
-
-    print(f'\n    10. Now search for "{Colors["Red"]}OEM Unlocking{Colors["Reset"]}" option in Developer options and {Colors["Green"]}ENABLE{Colors["Reset"]} it!')
-    print(
-        f'''
-        {Colors["Red"]}IF{Colors["Reset"]} YOU CANNOT FIND THAT OPTION THEN LOOK AT THESE DOCUMENTATION :
-        \t1. "{Colors["Blue"]}https://krispitech.com/fix-the-missing-oem-unlock-in-developer-options/{Colors["Reset"]}"
-        \t2. "{Colors["Blue"]}https://www.quora.com/Why-do-some-mobile-companies-refuse-to-unlock-bootloaders-like-Huawei-and-Realme{Colors["Reset"]}"
-        '''
-        )
-
-    input(f'\n\t=> Press {Colors["Green"]}Enter{Colors["Reset"]} key to continue : ')
-    print()
 
 
-SetupDeviceForUSBCommunication()
+from AutoRootUtilities import *
+
+
+
+def Download_Status(Status: str):
+    path = os.getcwd() + "\\Downloads\\"
+    versions = str(subprocess.check_output(f'samloader -m GT-I8190N -r XME checkupdate', stderr = subprocess.STDOUT, shell = True), encoding = 'utf-8')[:-2]
+    if Status == 'New Download':
+        try:
+            os.system(f'samloader --dev-model GT-I8190N --dev-region XME download --fw-ver {versions} --do-decrypt --out-dir {path}')
+        except ConnectionAbortedError:
+            print(f'Your {Colors["Red"]}internet connection{Colors["Reset"]} has been stopped or aborted!\nPlease {Colors["Green"]}check{Colors["Reset"]} your internet connection!')
+            input(f"Press {Colors['Green']}ENTER{Colors['Reset']} to confirm if your internet connection works : ")
+            if isConnected():
+                Download_Status(Status = 'Resume Downlad') 
+            else:
+                raise SystemExit()
+        
+        except Exception as ex:
+            print('Cannot start or continue {Phone.Model} firmware\'s download for an unknown error!')
+            input(f"Press {Colors['Green']}ENTER{Colors['Reset']} to exit : ")
+            raise SystemExit()
+
+    elif Status == 'Resume Download':
+        print(f'{Colors["Green"]}Resuming{Colors["Reset"]} the download...')
+        try:
+            os.system(f'samloader --dev-model GT-I8190N --dev-region XME download --resume --fw-ver {versions} --do-decrypt --out-dir {path}')
+
+        except ConnectionAbortedError:
+            print(f'Your internet connection has been stopped or aborted!\nPlease check your internet connection!')
+            input(f"Press {Colors['Green']}ENTER{Colors['Reset']} to confirm if your internet connection works : ")
+            if isConnected():
+                Download_Status(Status = 'Resume Downlad') 
+            else:
+                raise SystemExit()
+
+        except Exception as ex:
+            print('Cannot start or continue {Phone.Model} firmware\'s download for an unknown error!')
+            input(f"Press {Colors['Green']}ENTER{Colors['Reset']} to exit : ")
+            raise SystemExit()
+
+Download_Status('New Download')
