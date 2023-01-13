b9e27572f64fbd05c01d3193b13e3f8f1ae5005b
QuantumNone <jeamscrush@gmail.com>
2023-01-13 18:04:50 +0100

    Added Checkings and Device Informations
    Will add more informations to get from the device inside the dict.
    Added some Samsung Device checks...

--- AutoRooting/Main.py
+++ AutoRooting/Main.py
@@ -9,41 +9,6 @@ def Welcome_Screen():
     #...
     input('Rooting will erase all your phone data.\nContinue?')
 
-# The user has to follow these steps in order to be able to use Adb
-def SetupDeviceForUSBCommunication():
-    """User has to MANUALLY setup his device to start USB communication"""
-
-    def DelayedPrint(string: str, sleepDelay: float) -> None:
-        for line in string.split('\n'):
-            print(line)
-            sleep(sleepDelay)
-
-    instructions = f'''
-    1. Open your device {Colors["Green"]}settings{Colors["Reset"]} and navigate into "About my phone" option.
-    2. Search for "{Colors["Red"]}Build number{Colors["Reset"]}" option inside these settings (if you cannot find it try in "{Colors["Green"]}Software Information{Colors["Reset"]}" option).
-    3. Tap 7 times on "Build number" option to enable {Colors["Red"]}Developer Options{Colors["Reset"]}.
-    4. Go back to settings and {Colors["Red"]}search{Colors["Reset"]} for Developer Options.
-    5. Search for "{Colors["Red"]}USB debugging{Colors["Reset"]}" option and {Colors["Green"]}enable{Colors["Reset"]} it.
-    6. {Colors["Green"]}Connect{Colors["Reset"]} now your device to your computer trough USB cable and check your device screen.
-    7. {Colors["Green"]}Allow{Colors["Reset"]} the pop-up asking for computer permissions.
-    8. Now search inside Developer Options for "{Colors["Red"]}Select USB configuration{Colors["Reset"]}".
-    9. Click it and select "{Colors["Green"]}MTP File transfer{Colors["Reset"]}" protocol.'''
-
-    DelayedPrint(instructions, sleepDelay)
-
-    input(f'\n\t=> Press {Colors["Green"]}Enter{Colors["Reset"]} key to continue : ')
-
-    print(f'\n    10. Now search for "{Colors["Red"]}OEM Unlocking{Colors["Reset"]}" option in Developer options and {Colors["Green"]}ENABLE{Colors["Reset"]} it!')
-    print(
-        f'''
-        {Colors["Red"]}IF{Colors["Reset"]} YOU CANNOT FIND THAT OPTION THEN LOOK AT THESE DOCUMENTATION :
-        \t1. "{Colors["Blue"]}https://krispitech.com/fix-the-missing-oem-unlock-in-developer-options/{Colors["Reset"]}"
-        \t2. "{Colors["Blue"]}https://www.quora.com/Why-do-some-mobile-companies-refuse-to-unlock-bootloaders-like-Huawei-and-Realme{Colors["Reset"]}"
-        '''
-        )
-
-    input(f'\n\t=> Press {Colors["Green"]}Enter{Colors["Reset"]} key to continue : ')
-    print()
 
 
 
@@ -57,13 +22,14 @@ def SetupDeviceForUSBCommunication():
 
 #Else, use ADB to get device informations. This function is an example on how it should work (The dict should be a class)
 
-def GetPhoneInformations() -> dict[str]:
+def GetPhoneInformations() -> dict[str, str]:
     PhoneInformations = {
         'Model': 'ro.product.model',
         'Build Number': 'ro.build.display.id',
         'Android Version': 'ro.build.version.release',
         'PDA': 'ro.build.PDA', #SAMSUNG FIRMWARES NEED PDA and CSS
-        'CSC': 'ro.csc.country_code'
+        'CSC': 'ro.csc.country_code',
+        'Region': 'ro.build.version.codename' #[REL, TIM] italy
     }
     subprocess.call('cls', shell=True)
 
@@ -91,8 +57,27 @@ class Phone:
         self.AndroidVersion = PhoneInformations['Android Version']
         self.PDA = PhoneInformations['PDA']
         self.CSC = PhoneInformations['CSC']
+        self.Region = PhoneInformations['Region']
 
-# device = Phone()
+Device = Phone()
+
+if Device.Model == 'Samsung':
+    if int(Device.AndroidVersion[0]) < 9:
+        print(f'Your device is running on Android V{Device.AndroidVersion}, which is unsupported!')
+        print(f'{Colors["Green"]}Only{Colors["Reset"]} Android Version >= 9 is supported!')
+        input(f"Press {Colors['Green']}ENTER{Colors['Reset']} to exit : ")
+        raise SystemExit()
+
+    Samsung_Requirements(Phone())
+
+else:
+    print(f'''
+    Sorry!
+    This device is unsupported...
+    Cannot root {Device.Model}
+    ''')
+    input(f"Press {Colors['Green']}ENTER{Colors['Reset']} to exit : ")
+    raise SystemExit()
 
 
 
